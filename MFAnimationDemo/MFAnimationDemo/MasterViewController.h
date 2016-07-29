f1c8cdfe72d968b1f65aeede5f406f24476d49e9
胡淼枫 <humiaofeng@163.com>
2016-07-29 13:58:51 +0800

    Revert "delete"
    This reverts commit b8a2defe7ca84046032cc868d9e08f3e37bbce77.

---
+++ MFAnimationDemo/MFAnimationDemo/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  MFAnimationDemo
+//
+//  Created by 胡淼枫 on 16/7/29.
+//  Copyright © 2016年 胡淼枫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
